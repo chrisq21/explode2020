0eba32f4dd8185bab7035451712855ec24a44494
Chris Queen <chrisqueen10@gmail.com>
2020-12-30 17:45:27 -0500

    Added sitemetadata and removed styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  siteMetadata: {
+    title: `explode2020`,
+    siteUrl: `https://www.explode2020.com/`
+  },
   plugins: [
     "gatsby-plugin-emotion",
     "gatsby-plugin-react-helmet",
